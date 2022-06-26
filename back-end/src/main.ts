cd07bfa5eb25c15d7a30e6d6e9a21812a433f68c
Aghas2408 <aghasi.seyranyan@gmail.com>
2022-06-27 03:20:16 +0400

    enable cors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -6,6 +6,7 @@ async function start() {
   const port = process.env.PORT || 3000;
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, () => {
     console.log(`Server started on port ${port}`);
