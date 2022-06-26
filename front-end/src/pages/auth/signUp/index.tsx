e6b7492509108e9cbb53202bc571337335326d4b
Aghas2408 <aghasi.seyranyan@gmail.com>
2022-06-26 20:28:31 +0400

    Email and password validations

--- front-end/src/pages/auth/signUp/index.tsx
+++ front-end/src/pages/auth/signUp/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import DropDown from "../../../components/shared/dropDown";
 import { DROP_DOWN_ITEMS } from "../../../utils/constant/drop_down_items";
 
@@ -9,21 +9,81 @@ export default function SignUp() {
     e.preventDefault();
   };
 
+  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const enteredValue = event.target.value.trim();
+    setPassword(enteredValue);
+  };
+  const [isValid, setIsValid] = useState(false);
+  const [message, setMessage] = useState("");
+  const emailRegex =
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; /* eslint-disable-line */
+
+  const validateEmail = (event: any) => {
+    const email = event.target.value;
+    const isEmailRegex = emailRegex.test(email);
+    if (isEmailRegex) {
+      setIsValid(true);
+      setMessage("Email is correct!");
+    } else {
+      setIsValid(false);
+      setMessage("Please enter a valid email!");
+    }
+  };
+
+  const passValid = false;
+  const [passMessage, setPassMessage] = useState("");
+  const [password, setPassword] = useState<string>("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (password.length > 0 && password.length <= 7) {
+      setIsButtonDisabled(true);
+      setPassMessage("Please enter more than 8 symbols!");
+    } else if (password.length <= 16) {
+      setIsButtonDisabled(false);
+      setPassMessage("");
+    } else {
+      setPassMessage("Please enter less than 16 symbols!");
+      setIsButtonDisabled(true);
+    }
+  }, [password]);
+
   return (
     <div className="sign_up_container">
       <div className="sign_up_box">
         <h2>Sign Up</h2>
         <form onSubmit={handleSubmitForm}>
           <label htmlFor="email">
-            <input type="email" id="email" placeholder="Enter email" />
-            <p className="invalid_text"></p>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter email"
+              onChange={validateEmail}
+            />
+            <p
+              className={`message ${isValid ? "emailSuccess" : "invalid_text"}`}
+            >
+              {message}
+            </p>
           </label>
           <label htmlFor="password">
-            <input type="text" id="passwords" placeholder="Enter password" />
-            <p className="invalid_text"></p>
+            <input
+              type="text"
+              id="passwords"
+              placeholder="Enter password"
+              onChange={inputHandler}
+            />
+            <p className={`message ${!passValid && "invalid_text"}`}>
+              {passMessage}
+            </p>
           </label>
           <DropDown dropDownItems={DROP_DOWN_ITEMS} />
-          <button>Submit</button>
+          <button
+            disabled={isButtonDisabled}
+            className={isButtonDisabled ? "disabledButton" : "button"}
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
